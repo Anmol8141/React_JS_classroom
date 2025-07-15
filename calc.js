34917aef896ec6f0d4663318062a849b7c1d95a8
Anmol8141 <147474510+Anmol8141@users.noreply.github.com>
2025-07-15 23:37:54 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,9 +1,16 @@
 console.log("js page is intigrated successfully")
 function dis(n){
-    //var a= document.getElementById(n).value
-    console.log(`Button ${n} pressed`)
-    document.getElementById("result").value += n
+    //var a= document.getElementById(n).value;
+    console.log(`Button ${n} pressed`);
+    console.log(n);
+    document.getElementById("result").innerHTML += n;
 }
-function solve(String){
-    console.log(`Button ${String} pressed`)
+function solve(){
+    console.log(`Button  pressed`);
+    var s= document.getElementById("result").innerHTML;
+    console.log(s);
+    document.getElementById("result").innerHTML=eval(s);
+}
+function clr(){
+     document.getElementById("result").innerHTML="";
 }
