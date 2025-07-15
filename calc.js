a5ba36e8698ce59e8541157e7e1a85aff146787b
Anmol8141 <147474510+Anmol8141@users.noreply.github.com>
2025-07-15 11:57:03 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,9 @@
+console.log("js page is intigrated successfully")
+function dis(n){
+    //var a= document.getElementById(n).value
+    console.log(`Button ${n} pressed`)
+    document.getElementById("result").value += n
+}
+function solve(String){
+    console.log(`Button ${String} pressed`)
+}
